e7aa8481d30df1b001c685d0fe014e184c3c5fcd
Rakesh Kadulkar <rakesh.kadulkar@gmail.com>
2020-03-05 20:56:06 +0530

    -- updates tp Command.java and CommandUsingEnumSet.java

--- src/Command.java
+++ src/Command.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+// 1 ==> 0001 << 0 ==> 0001 ==> 1
+// 1 ==> 0001 << 1 ==> 0010 ==> 2
+// 1 ==> 0001 << 2 ==> 0100 ==> 4
+// 1 ==> 0001 << 3 ==> 1000 ==> 8
 
 public class Command {
+    public static final int DATA_HOLDINGS = (1 << 0);
+    public static final int DATA_NO_INVESTMENTS = (1 << 1);
+    public static final int DATA_FIND_HOLDING_NAMES = (1 << 2);
+    public static final int DATA_USES_MULTI_ROW_HEADERS = (1 << 3);
+
+    public static void main(String[] args) {
+        System.out.println("DATA_HOLDINGS = " + DATA_HOLDINGS);
+        System.out.println("DATA_NO_INVESTMENTS = " + DATA_NO_INVESTMENTS);
+        System.out.println("DATA_FIND_HOLDING_NAMES = " + DATA_FIND_HOLDING_NAMES);
+        System.out.println("DATA_USES_MULTI_ROW_HEADERS = " + DATA_USES_MULTI_ROW_HEADERS);
+
+
+/*
+        applyHints(DATA_HOLDINGS | DATA_NO_INVESTMENTS);
+        //0001
+        //0010
+        //-----
+        //0011
+
+        applyHints(DATA_HOLDINGS | DATA_FIND_HOLDING_NAMES);
+        applyHints(DATA_HOLDINGS | DATA_USES_MULTI_ROW_HEADERS);
+
+ */
+
+/*        System.out.println("Parsing data 1");
+        parseData("data", DATA_HOLDINGS | DATA_FIND_HOLDING_NAMES);
+
+        System.out.println("Parsing data 2");
+        parseData("data", DATA_FIND_HOLDING_NAMES | DATA_USES_MULTI_ROW_HEADERS);
+
+ */
+    }
+
+    public static void applyHints(int hints) {
+        System.out.println(hintsAreHoldingAndNoInvestment(hints));
+    }
+
+    public static boolean hintsAreHoldingAndNoInvestment(int hint_number) {
+        if ((hint_number == (DATA_HOLDINGS | hint_number)) && (hint_number == (DATA_NO_INVESTMENTS | hint_number))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void parseData(String data, int hint_number) {
+        if (hint_number == (DATA_HOLDINGS | hint_number)) {
+            System.out.println("Processing holdings");
+        }
+        if (hint_number == (DATA_FIND_HOLDING_NAMES | hint_number)) {
+            System.out.println("We have to find holding names");
+        }
+        if (hint_number == (DATA_USES_MULTI_ROW_HEADERS | hint_number)) {
+            System.out.println("We have to use multi row headers.");
+        }
+    }
 }
+
+
+
--- src/CommandUsingEnumSet.java
+++ src/CommandUsingEnumSet.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class CommandUsingEnumSet {
+
+    public enum HINTS {
+        DATA_HOLDINGS, DATA_NO_INVESTMENTS, DATA_FIND_HOLDING_NAMES, DATA_USES_MULTI_ROW_HEADERS;
+    }
+
+    public static void main(String[] args) {
+        Stream.of(HINTS.values()).forEach(System.out::println);
+/*
+        applyHints(EnumSet.of(HINTS.DATA_HOLDINGS ,HINTS.DATA_NO_INVESTMENTS));
+        applyHints(EnumSet.of(HINTS.DATA_HOLDINGS ,HINTS.DATA_FIND_HOLDING_NAMES));
+        applyHints(EnumSet.of(HINTS.DATA_HOLDINGS ,HINTS.DATA_USES_MULTI_ROW_HEADERS));
+*/
+
+/*
+        parseData("data", EnumSet.of(HINTS.DATA_HOLDINGS, HINTS.DATA_FIND_HOLDING_NAMES));
+        parseData("data", EnumSet.of(HINTS.DATA_FIND_HOLDING_NAMES, HINTS.DATA_USES_MULTI_ROW_HEADERS));
+*/
+
+    }
+
+    public static void applyHints(Set<HINTS> hints) {
+        System.out.println(hintsAreHoldingAndNoInvestment(hints));
+    }
+
+    public static boolean hintsAreHoldingAndNoInvestment(Set<HINTS> hints) {
+        if ( hints.size() != 2) {
+            return false;
+        }
+
+        long hints_count = hints.stream().filter(e -> (e == HINTS.DATA_HOLDINGS || e == HINTS.DATA_NO_INVESTMENTS)).count();
+        return hints_count == 2;
+    }
+
+    public static void parseData(String data, Set<HINTS> hints) {
+        for ( HINTS hint : hints ) {
+            switch(hint) {
+                case DATA_HOLDINGS:
+                    System.out.println("Processing holdings");
+                    break;
+                case DATA_FIND_HOLDING_NAMES:
+                    System.out.println("We have to find holding names");
+                    break;
+                case DATA_USES_MULTI_ROW_HEADERS:
+                    System.out.println("We have to use multi row headers.");
+                    break;
+            }
+        }
+    }
 }
